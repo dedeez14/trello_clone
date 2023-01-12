d773e27efc5e15bf80fa5cb17c0a466591a3f699
dedeez14 <febriansyahd65@gmail.com>
2023-01-12 13:15:44 +0700

    update penambahan HTML

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-<?php
-echo "yok bisa yok";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar Github</title>
+</head>
+<body>
+    
+</body>
+</html>
